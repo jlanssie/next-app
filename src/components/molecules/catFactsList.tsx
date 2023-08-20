1b4e624b1bf5e7449bb9c8a2bc1ffe9cbb78501c
Jeremy Lanssiers <jeremy.lanssiers@gmail.com>
2023-08-20 21:28:09 +0200

    main - Fetch API example

--- src/components/molecules/catFactsList.tsx
+++ src/components/molecules/catFactsList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import "./catFactsList.scss";
 
 export const CatFacts = () => {
   const [loading, setLoading] = useState(true);
